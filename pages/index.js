a0210d7a43c40059b9b9f31928ef92c2d268731f
pavlukha <kinomant@yandex.ru>
2020-10-14 16:55:04 +0300

    Create Wordpress template

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Head from 'next/head'
+// import Head from 'next/head'
 import Container from '../components/container'
 import MoreStories from '../components/more-stories'
 import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
+// import Intro from '../components/intro'
 import Layout from '../components/layout'
 import { getAllPostsForHome } from '../lib/api'
-import { CMS_NAME } from '../lib/constants'
+// import { CMS_NAME } from '../lib/constants'
 
 export default function Index({ allPosts: { edges }, preview }) {
   const heroPost = edges[0]?.node
@@ -14,11 +14,11 @@ export default function Index({ allPosts: { edges }, preview }) {
   return (
     <>
       <Layout preview={preview}>
-        <Head>
+        {/* <Head>
           <title>Next.js Blog Example with {CMS_NAME}</title>
-        </Head>
+        </Head> */}
         <Container>
-          <Intro />
+          {/* <Intro /> */}
           {heroPost && (
             <HeroPost
               title={heroPost.title}
